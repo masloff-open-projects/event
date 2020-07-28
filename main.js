18cbb6416de49408978ab620cc0c619d2963356b
eps-game <masloff@zoho.com>
2020-07-28 17:59:10 +0500

    Update

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 const ccxt = require ('ccxt');
 const W3CWebSocket = require('websocket').w3cwebsocket;
+const async = require ('async');
+
+const callback = {
+    on: {
+        update_price: function () {
+            console.log(global.data)
+        }
+    }
+}
+
+global.data = {
+    price: {
+        bybit: {
+            btc: 0
+        },
+        deribit: {
+            btc: 0
+        }
+    }
+}
+
+
 
 // Функция для цниверсального подключения к сокет соеденениям бирж
-function exchange_wss (wss='wss://', send="", callback=null, recursion=false) {
+function exchange_wss (wss='wss://', send="", callback=null, recursion=false, callback_on_update = false) {
 
     const client = new W3CWebSocket(wss);
 
     client.onerror = function() { throw 'Connection Error'; };
     client.onclose = function() { throw 'Client Closed'; };
     client.onopen = function() { client.send(send); };
-    client.onmessage = function(e) { callback(e.data); if (recursion) { client.send(send); }};
+    client.onmessage = function(e) { callback(e.data); if (recursion) { client.send(send); } if (callback_on_update) {callback_on_update(e.data)} };
 
 }
 
@@ -17,17 +39,17 @@ function exchange_wss (wss='wss://', send="", callback=null, recursion=false) {
 exchange_wss('wss://stream.bybit.com/realtime', JSON.stringify({
     op: "subscribe",
     args: [
-        'kline.BTCUSD.1m'
+        "trade.BTCUSD"
     ]
 }), function (e=null) {
 
     let response = JSON.parse(e);
 
-    if ('data' in response && 'close' in response.data) {
-        console.log('ByBit', response.data.close)
+    if ('data' in response && 'topic' in response && response.topic == 'trade.BTCUSD') {
+        global.data.price.bybit.btc = response.data[0].price;
     }
 
-})
+}, false, callback.on.update_price)
 
 // Deribit Socket
 exchange_wss('wss://www.deribit.com/ws/api/v1/', JSON.stringify({
@@ -37,7 +59,7 @@ exchange_wss('wss://www.deribit.com/ws/api/v1/', JSON.stringify({
     let response = JSON.parse(e);
 
     if ('success' in response && response.success == true && 'result' in response) {
-        console.log('Deribit', response.result.btc)
+        global.data.price.deribit.btc = response.result.btc;
     }
 
-}, true)
+}, true, callback.on.update_price)
