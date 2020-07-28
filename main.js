18f3f82ebf50eb75eae647f74563793bf8603f78
eps-game <masloff@zoho.com>
2020-07-28 17:12:55 +0500

    Update

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 const ccxt = require ('ccxt');
 const W3CWebSocket = require('websocket').w3cwebsocket;
 
-function exchange_wss (wss='wss://', send="", callback=null) {
+// Функция для цниверсального подключения к сокет соеденениям бирж
+function exchange_wss (wss='wss://', send="", callback=null, recursion=false) {
 
     const client = new W3CWebSocket(wss);
 
     client.onerror = function() { throw 'Connection Error'; };
     client.onclose = function() { throw 'Client Closed'; };
     client.onopen = function() { client.send(send); };
-    client.onmessage = function(e) { callback(e.data); };
+    client.onmessage = function(e) { callback(e.data); if (recursion) { client.send(send); }};
 
 }
 
-// exchange_wss('wss://stream.bybit.com/realtime', JSON.stringify({
-//     op: "subscribe",
-//     args: [
-//         'kline.BTCUSD.1m'
-//     ]
-// }), function (e=null) {
-//     console.log(e)
-// })
+// ByBit Socket
+exchange_wss('wss://stream.bybit.com/realtime', JSON.stringify({
+    op: "subscribe",
+    args: [
+        'kline.BTCUSD.1m'
+    ]
+}), function (e=null) {
 
+    let response = JSON.parse(e);
+
+    if ('data' in response && 'close' in response.data) {
+        console.log('ByBit', response.data.close)
+    }
+
+})
+
+// Deribit Socket
 exchange_wss('wss://www.deribit.com/ws/api/v1/', JSON.stringify({
     "action": "/api/v1/public/index"
 }), function (e=null) {
-    console.log(e)
-})
+
+    let response = JSON.parse(e);
+
+    if ('success' in response && response.success == true && 'result' in response) {
+        console.log('Deribit', response.result.btc)
+    }
+
+}, true)
